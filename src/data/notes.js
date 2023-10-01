6168d3b69ce1ffa5ab5d65d16f8c6bb687b040f9
Manolya Tamimi <tamimimanolya@gmail.com>
2023-10-01 21:35:31 +0300

    fixing date format

--- src/data/notes.js
+++ src/data/notes.js
@@ -3,53 +3,53 @@ export const notes = [
         title: 'my first note',
         content: 'green',
         color: 'green',
-        cDate: '20-10-2008'
+        cDate: '20/10/2008'
     },
     {
         title: 'my nth note',
         content: 'pink',
         color: 'pink',
-        cDate: '19-11-2012'
+        cDate: '19/11/2012'
     },
     {
         title: 'my nth note',
-        content: 'orange',
+        content: 'this is an orange cool weird colored card with an extra large text but what if it was even longer that usual, like longer than 100 pixels, what would happen, i wonder... this is an orange cool weird colored card with an extra large text but what if it was even longer that usual, like longer than 100 pixels, what would happen, i wonder',
         color: 'orange',
-        cDate: '19-11-2012'
+        cDate: '19/11/2012'
     },
     {
         title: 'my nth note',
         content: 'yellow',
         color: 'yellow',
-        cDate: '19-11-2012'
+        cDate: '19/11/2012'
     },
     {
         title: 'my nth note',
         content: 'blue',
         color: 'blue',
-        cDate: '19-11-2012'
+        cDate: '19/11/2012'
     },
     {
         title: 'my nth note',
         content: 'purple',
         color: 'purple',
-        cDate: '19-11-2012'
+        cDate: '19/11/2012'
     },
     {
         title: 'my nth note',
         content: 'transparent',
         color: 'transparent',
-        cDate: '19-11-2012'
+        cDate: '19/11/2012'
     },
     {
         title: 'my nth note',
         content: 'red',
         color: 'red',
-        cDate: '19-11-2012'
+        cDate: '19/11/2012'
     },
     {
         title: 'my nth note',
         content: 'no color is given',
-        cDate: '19-11-2012'
+        cDate: '19/11/2012'
     },
 ]
