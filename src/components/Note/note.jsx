a2ebe579846a8c70421035bf5eaa31fa1be28f78
Manolya Tamimi <tamimimanolya@gmail.com>
2023-10-02 22:56:20 +0300

    alowing note preview (without edit)

--- src/components/Note/note.jsx
+++ src/components/Note/note.jsx
@@ -1,12 +1,29 @@
 import './note.css';
 
 const Note = (props) => {
-    // call color -> className parser
+
+    // call color -> className parser?
+
+    const CloseHandler = (e) => {
+        e.stopPropagation();
+        props.closeNote();
+    }
+
     return (
-        <div className={`note ${props.color}`}>
+        <div
+            className={`note ${props.color} ${props.isSelected ? 'open-note' : ''}`}
+            onClick={props.openNote}
+        >
             <h4>{props.title}</h4>
             <p className='note-content'>{props.content}</p>
             <p className='note-date'>{props.date}</p>
+            {
+                props.isSelected &&
+                <div className='open-note-buttons'>
+                    <button onClick={CloseHandler}>close</button>
+                    <button>Done</button>
+                </div>
+            }
         </div>
     )
 }
--- src/components/NoteGrid/note-grid.jsx
+++ src/components/NoteGrid/note-grid.jsx
@@ -1,17 +1,23 @@
 import './note-grid.css';
 import Note from '../Note/note';
 import { notes } from '../../data/notes';
+import { useState } from 'react';
 
 const NoteGrid = () => {
+    const [openNote, setOpenNote] = useState(-1);
+
     return (
         <div className='note-grid'>
             {notes.map((note, index) =>
                 <Note
+                    isSelected={openNote === index}
+                    closeNote={() => setOpenNote(-1)}
                     key={`note#${index}`}
                     title={note.title}
                     content={note.content}
                     color={note.color}
                     date={note.cDate}
+                    openNote={() => setOpenNote(index)}
                 />
             )}
         </div>
