af199ef22178617e59293a11e1d8c2442f0c303b
Manolya Tamimi <tamimimanolya@gmail.com>
2023-10-05 22:08:31 +0300

    styling dialog

--- src/components/Dialog/confirm-dialog.jsx
+++ src/components/Dialog/confirm-dialog.jsx
@@ -4,9 +4,13 @@ const ConfirmDialog = (props) => {
     const { message, onReject, onConfirm } = props;
     return (
         <div className="confirm-dialog">
-            <p>{message}</p>
-            <button onClick={onReject}>cancel</button>
-            <button onClick={onConfirm}>ok</button>
+            <div className="confirm-dialog-innner">
+                <p>{message}</p>
+                <div className="confirm-dialog-button-group">
+                    <button onClick={onReject}>cancel</button>
+                    <button onClick={onConfirm}>ok</button>
+                </div>
+            </div>
         </div>
     )
 }
