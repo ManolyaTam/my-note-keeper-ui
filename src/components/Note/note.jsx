573f329efd3c0b1f3390b05c01b4982d94410cbd
Manolya Tamimi <tamimimanolya@gmail.com>
2023-10-05 20:48:07 +0300

    handling edit/readonly notes transition

--- src/components/Note/note.jsx
+++ src/components/Note/note.jsx
@@ -1,18 +1,25 @@
 import './note.css';
 
 const Note = (props) => {
+    const { index, isOpenNote, openNote, closeNote } = props;
+
+    const resetHandler = () => { closeNote(index) };
+    const editHandler = () => { closeNote(index) }
+    let color = isOpenNote(index) ? 'red' : '';
+
     return (
-        <div className='note'>
+        <div className='note' onClick={() => openNote(index)}>
             <form>
-                <div className="inner-note">
-                    <input className='title-input' placeholder='Title' />
-                    <textarea placeholder='Take a note...'></textarea>
-                    <div className='new-note-buttons'>
-                        <button>
-                            Close
-                        </button>
-                        <button type='submit'>Create</button>
-                    </div>
+                <div className="inner-note" style={{ borderColor: color }}>
+                    <input className='title-input' placeholder='Title' readOnly={!isOpenNote(index)} />
+                    <textarea placeholder='Take a note...' readOnly={!isOpenNote(index)}></textarea>
+                    {
+                        isOpenNote(index)
+                        && <div className='new-note-buttons'>
+                            <button type='reset' onClick={resetHandler}>Close</button>
+                            <button type='submit' onClick={editHandler}>Create</button>
+                        </div>
+                    }
                 </div>
             </form>
         </div>
