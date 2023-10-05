90e024a54220deb17ffa32ad72cd93d884a596d9
Manolya Tamimi <tamimimanolya@gmail.com>
2023-10-05 21:19:20 +0300

    passing and editing title and content in note

--- src/components/Note/note.jsx
+++ src/components/Note/note.jsx
@@ -1,17 +1,29 @@
+import { useState } from 'react';
 import './note.css';
 
 const Note = (props) => {
-    const { index, isOpenNote, openNote, closeNote, color } = props;
+    const { index, isOpenNote, openNote, closeNote, color, title, content } = props;
 
     const resetHandler = () => { closeNote(index) };
     const editHandler = () => { };
-
+    const [noteVals, setNoteVals] = useState({ title, content });
     return (
         <div className='note' onClick={() => openNote(index)}>
             <form>
                 <div className={`inner-note ${color}`}>
-                    <input className='title-input' placeholder='Title' readOnly={!isOpenNote(index)} />
-                    <textarea placeholder='Take a note...' readOnly={!isOpenNote(index)}></textarea>
+                    <input
+                        className='title-input'
+                        placeholder='Title'
+                        readOnly={!isOpenNote(index)}
+                        value={noteVals.title}
+                        onChange={(e) => setNoteVals({ ...noteVals, title: e.target.value })}
+                    />
+                    <textarea
+                        placeholder='Take a note...'
+                        readOnly={!isOpenNote(index)}
+                        value={noteVals.content}
+                        onChange={(e) => setNoteVals({ ...noteVals, content: e.target.value })}
+                    />
                     {
                         isOpenNote(index)
                         && <div className='new-note-buttons'>
