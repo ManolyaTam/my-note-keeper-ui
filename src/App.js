b150b5e4dcd5c8e9c28453e873aa7ab365980e3c
Manolya Tamimi <tamimimanolya@gmail.com>
2023-10-07 11:07:32 +0300

    adding searchparams in url

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ export const searchContext = createContext()
 
 function App() {
   const [notes, setNotes] = useState([]);
-  const [searchParam, setSearchParam] = useState('');
+  const urlParams = new URLSearchParams(window.location.search);
+  const [searchParam, setSearchParam] = useState(urlParams.get('search') || '');
 
   const fetchNotes = async () => {
     const fetchedNotes = await getAllNotes(searchParam);
@@ -17,9 +18,17 @@ function App() {
   }
 
   useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (searchParam.trim().length) {
+      urlParams.set('search', searchParam);
+      window.history.pushState({}, '', `/?search=${searchParam.toString().trim()}`);
+    } else {
+      urlParams.delete('search');
+      window.history.pushState({}, '', `/?${urlParams.toString().trim()}`);
+    }
     fetchNotes(searchParam);
-    // eslint-disable-next-line
-  }, [searchParam])
+    // eslint-disable-next-line 
+  }, [searchParam]); // missing dependency: fetchNotes?
 
   return (
     <div className="App">
